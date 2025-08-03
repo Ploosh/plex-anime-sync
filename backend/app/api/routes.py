5c9b3cbad5904262e8bd4e7f6abe004a135bd2a3
Gustavo Santos <gustavosisa@outlook.com>
2025-08-03 11:01:11 -0300

    feat: config settings and request from plex

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -1,16 +1,23 @@
+from typing import Any
+
 from fastapi import APIRouter
-from app.services.plex import get_recently_watched
-from app.services.mal import update_anime_from_plex
+
+from app.services import plex
+
+# from app.services.mal import update_anime_from_plex
 
 router = APIRouter()
 
-@router.get("/sync")
-def sync_watched():
-    results = get_recently_watched()
-    updates = update_anime_from_plex(results)
-    return {"synced": updates}
 
-@router.get("/auth/callback")
-def mal_callback(code: str):
-    token_info = exchange_code_for_token(code)
-    return token_info
+@router.get("/sync")
+def sync_watched(plex_client: plex.PlexClient) -> dict[str, list[dict[str, Any]]]:
+    results = plex.get_recently_watched(plex_client)
+    return {"recently_watched": results}
+    # updates = update_anime_from_plex(results)
+    # return {"synced": updates}
+
+
+# @router.get("/auth/callback")
+# def mal_callback(code: str):
+#     token_info = exchange_code_for_token(code)
+#     return token_info
--- backend/app/services/plex.py
+++ backend/app/services/plex.py
@@ -1,14 +1,40 @@
-from plexapi.server import PlexServer
-import os
+from typing import Annotated, cast
 
-def get_recently_watched():
-    plex = PlexServer(os.getenv("PLEX_URL"), os.getenv("PLEX_TOKEN"))
-    history = plex.account().watchHistory()
-    return [
-        {
-            "title": item.title,
-            "episodes_watched": 1,
-            "rating": 8  # hardcoded or inferred
-        }
-        for item in history if 'anime' in item.librarySectionTitle.lower()
-    ]
+from fastapi import Depends
+from plexapi.base import MediaContainer
+from plexapi.server import PlexServer
+from plexapi.video import EpisodeHistory
+
+from app.config import AppConfig
+
+
+def get_plex_server(config: AppConfig):
+    return PlexServer(config.plex_url, config.plex_token)
+
+
+PlexClient = Annotated[PlexServer, Depends(get_plex_server)]
+
+
+def get_recently_watched(plex: PlexClient):
+    history: MediaContainer[EpisodeHistory] = cast(
+        MediaContainer[EpisodeHistory], plex.history()
+    )
+    if not history:
+        return []
+
+    watched = {}
+
+    for item in history:
+        key = item.grandparentTitle if hasattr(item, "grandparentTitle") else item.title
+        if not key:
+            continue
+        found = watched.get(key)
+        if found:
+            found["episodes_watched"] += 1
+        else:
+            watched[key] = {
+                "title": item.title if item.title else item.grandparentTitle,
+                "episodes_watched": 1,
+            }
+
+    return list(watched.values())
